7abd97a0e45b6dfbf9e1166f169b84e02ade6d78
TemaSysoev <artyom.sysoev@gmail.com>
2018-04-21 16:34:49 +0300

    Initial Commit

---
+++ MathBOOM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MathBOOM
+//
+//  Created by Tema Sysoev on 21.04.2018.
+//  Copyright © 2018 Tema Sysoev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
