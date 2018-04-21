74279ecbd655446d5f0cb5d7932e1ccae72b069d
TemaSysoev <artyom.sysoev@gmail.com>
2018-04-21 16:57:59 +0300

    Маска и первый код
    Добавлен label для примера и кнопки

--- MathBOOM/ViewController.swift
+++ MathBOOM/ViewController.swift
@@ -9,9 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainLabel: UILabel!
+    var a1 = arc4random_uniform(10)
+    var a2 = arc4random_uniform(10)
+    var b = UInt32()
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Пишем механизм
+        b = a1 + a2
+        mainLabel.text = "\(a1) + \(a2)"
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
